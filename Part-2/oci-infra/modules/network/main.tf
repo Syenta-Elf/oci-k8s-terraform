7b5bbb307058b8967e8c5993814fd4bebf85b25f
Elif Nur Telaşe <elifnurtelase@192.168.1.17>
2026-04-04 22:59:41 +0300

    feat: add internet gateway and route table

--- Part-2/oci-infra/modules/network/main.tf
+++ Part-2/oci-infra/modules/network/main.tf
@@ -11,4 +11,24 @@ resource "oci_core_subnet" "test_subnet" {
     vcn_id = oci_core_vcn.test_vcn.id
     cidr_block = var.subnet_cidr_block
     display_name = var.subnet_display_name
+    route_table_id = oci_core_route_table.test_route_table.id
+}
+
+resource "oci_core_internet_gateway" "test_internet_gateway" {
+    #Required
+    compartment_id = var.tenancy_ocid
+    vcn_id = oci_core_vcn.test_vcn.id
+}
+
+resource "oci_core_route_table" "test_route_table" {
+    #Required
+    compartment_id = var.tenancy_ocid
+    vcn_id = oci_core_vcn.test_vcn.id
+        route_rules {
+        #Required
+        network_entity_id = oci_core_internet_gateway.test_internet_gateway.id
+
+        #Optional
+        destination = "0.0.0.0/0"
+    }
 }
